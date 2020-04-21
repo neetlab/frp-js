8cd51d92f4e991dd5a7da98887c431b1d438505e
Ryō Igarashi <n33t5hin@gmail.com>
2020-04-22 02:02:56 +0900

    なるほどね

--- src/publish.ts
+++ src/publish.ts
@@ -1,9 +1,13 @@
-import { range, interval, zip } from "rxjs";
-import { publish, map, tap } from "rxjs/operators";
+import { interval, ConnectableObservable, zip, range } from "rxjs";
+import { publish, mapTo, map } from "rxjs/operators";
 
-zip(range(1, 10), interval(1000)).pipe(
-  map((vs) => vs[1]),
-  publish(
-    tap(console.log),
-  ),
-).subscribe()
+const stream$ = zip(interval(1000), range(0, 10))
+  .pipe(map(([_, num]) => num));
+
+// https://github.com/ReactiveX/rxjs/issues/2972
+const stuff$ = stream$.pipe(publish()) as ConnectableObservable<number>;
+// ----------
+
+stuff$.connect();
+stuff$.subscribe(n => console.log(`1st: ${n}`));
+stuff$.subscribe(n => console.log(`2nd: ${n}`));
